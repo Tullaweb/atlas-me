27dd6b730f0013bc82ed62f96c35b9e09602e12b
Tullaweb <rnematullo93@gmail.com>
2023-03-08 13:34:03 +0500

    generetion

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,10 @@ module.exports = {
       xl:'1440px',
     },
     extend: {
+      maxWidth: {
+      '8xl': '1440px',
+      'md': '440px',
+      },
       colors: {
         Atlassky: '#12B0FA',
         Atlasdark: '#667085',
@@ -17,16 +21,19 @@ module.exports = {
         pegrayligt: '#EAECF0',
         footdark: '#1D2939',
         footcard: '#244086',
+        moon: '#FDB022',
       },
-     
       fontFamily: {
         'Inter': ['Inter', 'sans-serif'],
       },
-
       backgroundImage: {
         hero: "url('../imeges/bg-head-223.png')",
         first: "url('../imeges/bg-head.png')",
-      }
+      },
+      boxShadow: {
+        'card': '0px 12px 84px rgba(29, 41, 63, 0.119946)',
+        'shadmoon':  '0px 10px 10px -5px rgba(16, 24, 40, 0.03)',
+      },
     },
   },
   plugins: [],
